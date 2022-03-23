c0febd401244d8372fe4118cde6f13b0a3b21ef4
shiroocraziiphat <99143773+shiroocraziiphat@users.noreply.github.com>
2022-03-23 22:40:54 +0700

    Update weekly.js

--- plugins/weekly.js
+++ plugins/weekly.js
@@ -48,7 +48,7 @@ function button(teks, user) {
     
     const buttonMessage = {
         contentText: teks,
-        footerText: '©games-wabot',
+        footerText: '©shiro-md',
         buttons: buttons,
         headerType: 1
     }
